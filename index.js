2fdb60c944939c1dd2ef70c04aadbfd6e14bcf9d
chspur <stw51924@gmail.com>
2024-12-26 22:08:24 -0800

    updated index.html and index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const count = document.querySelector(".visitor-count");
 async function updateCount() {
     let response = await fetch("https://fywjpeisef63fc3uqgpfwwiuua0nkoav.lambda-url.us-west-1.on.aws/");
     let data = await response.json();
-    count.innerHTML = ` Views: ${data}`;
+    count.innerHTML = `Views: ${data}`;
 }
 
 updateCount();
